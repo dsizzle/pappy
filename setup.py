d96198c9fa825585bdbaf365017fa1a3d418cece
Dale Cieslak <dcieslak@yahoo.com>
2015-08-02 14:08:25 -0700

    Inital pull from old SourceForge project

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from Pyrex.Distutils.extension import Extension
+from Pyrex.Distutils import build_ext
+
+setup(name='pappy',
+	version='0.91',
+	ext_modules=[									\
+		Extension(									\
+			'pappy',								\
+			['pappy.pyx'],							\
+			libraries=['portaudio'],				\
+		)],
+	cmdclass = {'build_ext': build_ext},			\
+	description='Python Distribution Utilities',	\
+	author='Dale Cieslak',							\
+	author_email='desizzle@users.sourceforge.net',	\
+	url='http://pappy.sourceforge.net/',			\
+)
+
+
